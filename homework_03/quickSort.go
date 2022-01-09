4e5e60a9aefc09dd9ee5cc4e9c2c119bc818b917
trypophib1a <trypophib1a@mail.ru>
2022-01-10 03:26:57 +0500

    homework 03

---
+++ homework_03/quickSort.go
@@ -0,0 +1,39 @@
+package homework_03
+
+func QuickSort(arr []int) []int {
+	return fullSorted(arr, 0, len(arr)-1)
+}
+
+func fullSorted(arr []int, l, r int) []int {
+	if l >= r {
+		return arr
+	}
+
+	index := partition(arr, l, r)
+	fullSorted(arr, index, r)
+
+	return fullSorted(arr, l, index-1)
+}
+
+func partition(arr []int, l, r int) int {
+	pivotPosition := (l + r) / 2
+	pivot := arr[pivotPosition]
+
+	for l <= r {
+		for arr[l] < pivot {
+			l++
+		}
+
+		for arr[r] > pivot {
+			r--
+		}
+
+		if l <= r {
+			arr[l], arr[r] = arr[r], arr[l]
+			l++
+			r--
+		}
+	}
+
+	return l
+}
---
+++ homework_03/quickSort_test.go
@@ -0,0 +1,25 @@
+package homework_03
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQs(t *testing.T) {
+	tests := []struct {
+		input    []int
+		expected []int
+	}{
+		{input: []int{2, 3, 1, 4}, expected: []int{1, 2, 3, 4}},
+		{input: []int{7, 2, 3, -1, 0, 4}, expected: []int{-1, 0, 2, 3, 4, 7}},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(fmt.Sprintf("%+v", tc.input), func(t *testing.T) {
+			result := QuickSort(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
