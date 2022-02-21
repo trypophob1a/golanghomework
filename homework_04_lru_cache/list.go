05cd82b0bb48d8e463091d415311c24539f50183
trypophib1a <trypophib1a@mail.ru>
2022-02-21 23:47:32 +0500

    linked list implementation

--- homework_04_lru_cache/list.go
+++ homework_04_lru_cache/list.go
@@ -17,10 +17,134 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	front *ListItem
+	back  *ListItem
+	size  int
+}
+
+func (l list) Len() int {
+	return l.size
+}
+
+func (l list) IsEmpty() bool {
+	return l.size < 1
+}
+
+func (l list) Front() *ListItem {
+	return l.front
+}
+
+func (l list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newListItem := ListItem{v, nil, nil}
+
+	if l.IsEmpty() {
+		l.back = &newListItem
+	} else {
+		currentItem := l.front
+		newListItem.Next = currentItem
+		currentItem.Prev = &newListItem
+	}
+
+	l.front = &newListItem
+	l.size++
+
+	return l.front
+}
+func (l *list) PushBack(v interface{}) *ListItem {
+	newListItem := ListItem{v, nil, nil}
+
+	if l.IsEmpty() {
+		l.front = &newListItem
+	} else {
+		l.back.Next = &newListItem
+		newListItem.Prev = l.back
+	}
+
+	l.back = &newListItem
+	l.size++
+
+	return l.back
+}
+func (l *list) Remove(i *ListItem) {
+	if l.IsEmpty() {
+		return
+	}
+
+	if l.size < 2 {
+		l.front = nil
+		l.back = nil
+		l.size--
+		return
+	}
+
+	if i.Prev == nil {
+		l.RemoveFront()
+		return
+	}
+	if i.Next == nil {
+		l.RemoveBack()
+		return
+	}
+
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+
+	l.size--
+}
+
+func (l *list) RemoveFront() {
+	if l.IsEmpty() {
+		return
+	}
+
+	l.front = l.front.Next
+	l.front.Prev = nil
+
+	l.size--
+}
+
+func (l *list) RemoveBack() {
+	if l.IsEmpty() {
+		return
+	}
+
+	l.back.Prev.Next = nil
+	l.back = l.back.Prev
+
+	l.size--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i.Prev == nil {
+		return
+	}
+
+	if i.Next == nil {
+		front := l.front
+		i.Prev.Next = nil
+		l.back = i.Prev
+		i.Prev = nil
+		front.Prev = i
+		i.Next = front
+		l.front = i
+		return
+
+	}
+
+	i.Prev.Next = i.Next
+	i.Next.Prev = i.Prev
+	i.Prev = nil
+	front := l.front
+	i.Next = front
+	l.front = i
+	front.Prev = l.front
+
 }
 
 func NewList() List {
-	return new(list)
+	return &list{size: 0, back: nil, front: nil}
 }
--- homework_04_lru_cache/list_test.go
+++ homework_04_lru_cache/list_test.go
@@ -19,6 +19,9 @@ func TestList(t *testing.T) {
 		l := NewList()
 
 		l.PushFront(10) // [10]
+		l.Remove(l.Front())
+
+		l.PushFront(10) // [10]
 		l.PushBack(20)  // [10, 20]
 		l.PushBack(30)  // [10, 20, 30]
 		require.Equal(t, 3, l.Len())
@@ -26,6 +29,16 @@ func TestList(t *testing.T) {
 		middle := l.Front().Next // 20
 		l.Remove(middle)         // [10, 30]
 		require.Equal(t, 2, l.Len())
+
+		l.PushFront(500) // [500, 10, 30]
+		require.Equal(t, 500, l.Front().Value)
+		l.Remove(l.Front()) // [10, 30]
+		require.Equal(t, 10, l.Front().Value)
+
+		l.PushBack(500) // [10, 30, 500]
+		require.Equal(t, 500, l.Back().Value)
+		l.Remove(l.Back()) // [10, 30]
+		require.Equal(t, 30, l.Back().Value)
 
 		for i, v := range [...]int{40, 50, 60, 70, 80} {
 			if i%2 == 0 {
@@ -47,5 +60,19 @@ func TestList(t *testing.T) {
 			elems = append(elems, i.Value.(int))
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
+
+		l.MoveToFront(l.Front().Next)
+		require.Equal(t, 80, l.Front().Value) //80, 70, 60, 40, 10, 30, 50
+
+		l.MoveToFront(l.Front().Next.Next)
+		require.Equal(t, 60, l.Front().Value)                //60, 80, 70, 40, 10, 30, 50
+		require.Equal(t, 40, l.Front().Next.Next.Next.Value) //60, 80, 70, 40, 10, 30, 50
+
+		l.MoveToFront(l.Back().Prev.Prev)
+		require.Equal(t, 10, l.Front().Value)      //10, 60, 80, 70, 40, 30, 50
+		require.Equal(t, 60, l.Front().Next.Value) //10, 60, 80, 70, 40, 30, 50
+		l.MoveToFront(l.Back())
+		require.Equal(t, 30, l.Back().Value) //50, 10, 60, 80, 70, 40, 30
+
 	})
 }
